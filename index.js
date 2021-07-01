770ea012144c0950951ee547a8d681d06174e8c5
Mansi Padgelwar <74247233+mansipadgelwar@users.noreply.github.com>
2021-07-01 13:05:31 +0000

    feat: validate input

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ var readlineSync = require('readline-sync');
 var chalk = require("chalk");
 
 
-var userName = readlineSync.question('May I have your name?');
+var userName = readlineSync.question('May I have your name? ');
 
 console.log("\n \nHi " + userName + " enter your birth date and \nI will tell you if your birth date is prime or not. \n ");
 
-var userBirthDate = readlineSync.question('Enter your birth date in this format : DD/MM ');
+var userBirthDate = readlineSync.question('Enter your birth date in this format : DD/MM - ');
 
-var birthDate = split.userBirthDate('/');
+var numberOfDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+var birthDate = userBirthDate.split('/');
 var DD = birthDate[0];
 var MM = birthDate[1];
 
+if( DD < 31 || MM < 12 || DD > 0 || MM > 0 || (!isNaN(DD)) || (!isNaN(MM))){
+  if( DD < numberOfDays[MM-1] ){
+    console.log("valid");
+  }
+  else{
+    console.log(" ERROR !! INVALID DATE. ");
+  }
+   
+}
+else{
+  console.log(" ERROR !! INVALID DATE. ");
+}
