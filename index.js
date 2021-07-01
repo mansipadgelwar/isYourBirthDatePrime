c1a0d449d4eaee52fc4190f850d168f779afc462
Mansi Padgelwar <74247233+mansipadgelwar@users.noreply.github.com>
2021-07-01 13:27:24 +0000

    feat: check if prime

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var userName = readlineSync.question('May I have your name? ');
 
 console.log("\n \nHi " + userName + " enter your birth date and \nI will tell you if your birth date is prime or not. \n ");
 
-var userBirthDate = readlineSync.question('Enter your birth date in this format : DD/MM - ');
+var userBirthDate = readlineSync.question('Enter your birth date in this format : DD/MM - \n');
 
 var numberOfDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
@@ -16,7 +16,7 @@ var MM = birthDate[1];
 
 if( DD < 31 || MM < 12 || DD > 0 || MM > 0 || (!isNaN(DD)) || (!isNaN(MM))){
   if( DD < numberOfDays[MM-1] ){
-    console.log("valid");
+    checkPrime(DD);
   }
   else{
     console.log(" ERROR !! INVALID DATE. ");
@@ -25,4 +25,32 @@ if( DD < 31 || MM < 12 || DD > 0 || MM > 0 || (!isNaN(DD)) || (!isNaN(MM))){
 }
 else{
   console.log(" ERROR !! INVALID DATE. ");
+}
+
+function checkPrime(date){
+   let isPrime = true;
+   if( date > 1)
+   {
+     for( let i = 2; i < date ; i++)
+     {
+       if(date % i == 0)
+       {
+         isPrime = false;
+         break;
+       }
+      }
+
+      if(isPrime)
+      {
+         console.log("\n \nHey, your birth date is a prime number");
+      }
+      else
+      {
+         console.log("\n \nHey, your birth date is not a prime number");
+      }
+     }
+     else
+     {
+       console.log(" \n \nHey, your birth date is not a prime number");
+     } 
 }
